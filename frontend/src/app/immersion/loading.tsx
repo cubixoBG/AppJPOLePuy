4c9ce4059485b987d6806e577fed79839cbc7f84
AltTaPaul2 <Paul.charpentier2@etu.uca.fr>
2026-03-04 15:10:21 +0100

    App V3

---
+++ frontend/src/app/immersion/loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div className="loading-wrapper">
+      <div className="spinner">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <p className="loading-text">Chargement en cours…</p>
+
+     
+    </div>
+  );
+}
