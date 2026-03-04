5cddf418b6ac16bb83942b934f2702952261a25f
cubixo <cubixo2302@gmail.com>
2026-03-04 11:13:02 +0100

    section du choix de département designer front

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,13 +14,38 @@ export default function Home() {
             <br /><span>Site  du Puy-en-Velay</span></h1>
           <p>Nous sommes ravis de vous accueillir ! Inscrivez-vous en quelques
             clics pour profiter pleinement de votre visite.</p>
-          <ButtonFull texte="Commencer ➔" lien="#accueil_ChoixDep" />
+          <ButtonFull texte="Commencer ➔" lien="#accueil_Bottom" />
         </div>
       </section>
-      <section className={styles.accueil_ChoixDep} id="accueil_ChoixDep">
-        <SectionImmersion/>
+      <section className={styles.accueil_Bottom} id="accueil_Bottom">
+        <SectionImmersion />
 
-        
+        <div className={styles.choixDep}>
+          <section className={styles.choixDep_Container}>
+            <h2>Choisissez un département</h2>
+            <p>Sélectionnez le département qui vous intéresse le plus pour commencer votre inscription.</p>
+            <div>
+              <a href="#"><article>
+                <img src="/paletteCouleur.webp" alt="icon MMI" />
+                <h3>MMI</h3>
+                <p>Métiers du Multimédia
+                  et de l'Internet</p>
+              </article></a>
+              <a href="#"><article>
+                <img src="/ordinateur.webp" alt="icon Info" />
+                <h3>Informatique</h3>
+                <p>Développement,
+                  réseaux et systèmes</p>
+              </article></a>
+              <a href="#"><article>
+                <img src="chimie.webp" alt="icon Chimie" />
+                <h3>Chimie</h3>
+                <p>Chimie analytique et de
+                  synthèse</p>
+              </article></a>
+            </div>
+          </section>
+        </div>
       </section>
     </main>
   );
